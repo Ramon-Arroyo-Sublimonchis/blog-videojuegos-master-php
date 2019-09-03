add78ac081b79850f5022a190890a9b3197bb48c
Ramon Arroyo <fusionevolutiva@outlook.com>
2019-09-03 08:42:02 -0500

    Comenzamos el blog de videojuegos

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset = "utf-8"/>
+    <title>Blog de Videojuegos</title>
+
+
+</head>
+
+
+<body>
+
+<header>
+    <div>
+        <a href = "index.php">
+            <h1>Blog de Videojuegos</h1>
+        
+        </a>
+
+
+    </div>
+
+</header>
+
+</body>
+
+
+
+
+
+
+
+
+</html>   
