0eac3efff06cdfc17ba4875c71a31b531e7cb04f
Ramon Arroyo <fusionevolutiva@outlook.com>
2019-09-03 09:04:50 -0500

    Barra de navegacion basica

--- index.php
+++ index.php
@@ -13,17 +13,60 @@
 
 <body>
 
-<header>
+<header id="header">
     <div>
+
+        <!--LOGO -->
         <a href = "index.php">
-            <h1>Blog de Videojuegos</h1>
+            Blog de Videojuegos
         
         </a>
 
 
     </div>
 
+    <!-- MENU -->
+
+    <nav id="nav">
+    
+        <ul>
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+
+            <li>
+                <a href="#">CATEGORIA 1 </a>
+            </li>
+            <li>
+                <a href="#">CATEGORIA 2 </a>
+            </li>
+            <li>
+                <a href="#">CATEGORIA 3 </a>
+            </li>
+            <li>
+                <a href="#">CATEGORIA 4 </a>
+            </li>
+            <li>
+                <a href="#">Sobre Mi </a>
+            </li>
+            <li>
+                <a href="#">Contacto</a>
+            </li>
+
+
+
+
+
+        </ul>
+    
+    </nav>
+
+
 </header>
+
+
+    <!--BARRA LATERAL -->
+    <!--CAJA PRINCIPAL-->
 
 </body>
 
