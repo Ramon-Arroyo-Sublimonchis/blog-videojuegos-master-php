b48299a4d426aeebe4a4c45e890d347fca1afefe
Ramon Arroyo <fusionevolutiva@outlook.com>
2019-09-17 10:59:09 -0500

    Acomodamos los Includes

---
+++ includes/cabecera.php
@@ -0,0 +1,72 @@
+<?php require_once 'includes/conexion.php' ?>
+
+
+
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+    <meta charset = "utf-8"/>
+    <title>Blog de Videojuegos</title>
+    <link rel="stylesheet" type="text/css" href="./assets/css/estilos.css"/>
+
+
+</head>
+
+
+<body>
+
+<header id="cabecera">
+    <div id="logo">
+
+        <!--LOGO -->
+        <a  href = "index.php">
+            Blog de Videojuegos
+        
+        </a>
+
+
+    </div>
+
+    <!-- MENU -->
+
+    <nav id="menu">
+    
+        <ul>
+            <li>
+                <a href="#">Inicio</a>
+            </li>
+
+            <li>
+                <a href="#">CATEGORIA 1 </a>
+            </li>
+            <li>
+                <a href="#">CATEGORIA 2 </a>
+            </li>
+            <li>
+                <a href="#">CATEGORIA 3 </a>
+            </li>
+            <li>
+                <a href="#">CATEGORIA 4 </a>
+            </li>
+            <li>
+                <a href="#">Sobre Mi </a>
+            </li>
+            <li>
+                <a href="#">Contacto</a>
+            </li>
+
+
+
+
+
+        </ul>
+    
+    </nav>
+
+    <div class="clearfix"></div>
+</header>
+
+<div id = "contenedor">
--- index.php
+++ index.php
@@ -1,128 +1,11 @@
-<!DOCTYPE html>
-
-<html lang="es">
-
-<head>
-
-    <meta charset = "utf-8"/>
-    <title>Blog de Videojuegos</title>
-    <link rel="stylesheet" type="text/css" href="./assets/css/estilos.css"/>
-
-
-</head>
-
-
-<body>
-
-<header id="cabecera">
-    <div id="logo">
-
-        <!--LOGO -->
-        <a  href = "index.php">
-            Blog de Videojuegos
-        
-        </a>
-
-
-    </div>
-
-    <!-- MENU -->
-
-    <nav id="menu">
-    
-        <ul>
-            <li>
-                <a href="#">Inicio</a>
-            </li>
-
-            <li>
-                <a href="#">CATEGORIA 1 </a>
-            </li>
-            <li>
-                <a href="#">CATEGORIA 2 </a>
-            </li>
-            <li>
-                <a href="#">CATEGORIA 3 </a>
-            </li>
-            <li>
-                <a href="#">CATEGORIA 4 </a>
-            </li>
-            <li>
-                <a href="#">Sobre Mi </a>
-            </li>
-            <li>
-                <a href="#">Contacto</a>
-            </li>
-
-
-
-
-
-        </ul>
-    
-    </nav>
-
-
-</header>
-
-    <div class="clearfix"></div>
+<?php include_once 'includes/cabecera.php'; ?>
+   
     
 
-    <div id = "contenedor">
+    
     <!--BARRA LATERAL -->
 
-        <aside id = "sidebar">
-            <div id="login" class="bloque">
-            
-                <h3>Identificate</h3>
-                
-                    <form action="login.php" method = "POST" >
-                    
-                    <label for = "email">Email</label>
-                    <input type="email" name="email"/>
-
-                    <label for ="password">Contrasena</label>
-                    <input type="password" name="password"/>
-
-                    <input type="submit" value ="Entrar"/>
-
-
-
-                    </form>
-
-
-            </div>  
-
-
-            <div id="register" class="bloque">
-            
-                <h3>Registrate</h3>
-                
-                    <form action="registro.php" method = "POST" >
-                    
-                    <label for = "nombre">Nombre</label>
-                    <input type="text" name="nombre"/>
-                    
-                    <label for = "apellidos">Apellidos</label>
-                    <input type="text" name="apellidos"/>
-
-                    <label for = "email">Email</label>
-                    <input type="email" name="email"/>
-
-                    <label for ="password">Contrasena</label>
-                    <input type="password" name="password"/>
-
-                    <input type="submit" value ="Entrar"/>
-
-
-
-                    </form>
-
-
-            </div>  
-        
-        
-        </aside>
+       <?php require_once 'includes/lateral.php'; ?>
         <!--CAJA PRINCIPAL-->
 
         <div id ="principal">
@@ -159,28 +42,13 @@
         
 
 
-        <div class="clearfix"></div>
-    
-    
-    </div>
-
-    <footer id="pie">
-    <p>Desarrollado por Ramon Arroyo Sublimonchis &copy 2019 </p>
-    
-    </footer>
+  
 
 
-
-
+    <?php require_once 'includes/footer.php'; ?>
     
 
 </body>
-
-
-
-
-
-
 
 
 </html>   
