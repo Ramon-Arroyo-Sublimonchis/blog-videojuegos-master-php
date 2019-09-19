544f02ef5c1c48ff5aa79eeab0736e06770369a8
Ramon Arroyo <fusionevolutiva@outlook.com>
2019-09-19 09:53:28 -0500

    Terminamos las validadciones de ingreso usuario

--- registro.php
+++ registro.php
@@ -12,16 +12,55 @@ if(isset($_POST)){
 
     //Arrar de Errores
 
-    
+    $errores = array();
 
     //Validar datos antes de guardarlos en la base de datos
 
+    //Validar campo nombre
+
     if(!empty($nombre) && !is_numeric($nombre) && !preg_match("/[0-9]/", $nombre) ){
 
-        echo "El nombre es valido";
+        $nombre_validado = true;
     } else {
-        echo "Lo campos numericos son invalidos";
+        $nombre_validado = false;
+        $errores['nombre'] = "El Nombre no es Valido";
     }
+
+    //Validar Apellidos
+    if(!empty($apellidos) && !is_numeric($apellidos) && !preg_match("/[0-9]/", $apellidos) ){
+
+        $apellidos_validado = true;
+    } else {
+        $apellidos_validado = false;
+        $errores['apellidos'] = "Los Apellidos no son Validos";
+    }
+
+
+    //Validar el Mail
+    if(!empty($apellidos) && filter_var($email , FILTER_VALIDATE_EMAIL) ){
+
+        $email_validado = true;
+    } else {
+        $email_validado = false;
+        $errores['email'] = "El Email no es Valido";
+    }
+
+      //Validar el Password
+      if(!empty($password) ){
+
+        $password_validado = true;
+    } else {
+        $password_validado = false;
+        $errores['password'] = "La contrasena esta vacia";
+    }
+
+$guardar_usuario = false;
+
+if( count($errores) == 0 ){
+    $guardar_usuario = true;
+    //INSERTAR USUARION EN LA BASE DE DATOS
+} 
+
 
 
 
