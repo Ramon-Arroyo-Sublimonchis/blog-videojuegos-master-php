afa1bcc438e2f8258c548fa63196ec175aa91b76
Ramon Arroyo <fusionevolutiva@outlook.com>
2019-09-26 09:17:25 -0500

    Escapamos los string con comillas

--- registro.php
+++ registro.php
@@ -6,15 +6,18 @@ if(isset($_POST)){
     //Conexion a la Base de Datos
     require_once 'includes/conexion.php';
 
+    
+    if(isset($_SESSION)){
     session_start();
+    }
 
-
+    
 
     //Recoger los valores del Formulario de Registro
-    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : false;
-    $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
-    $email = isset($_POST['email']) ? $_POST['email'] : false;
-    $password = isset($_POST['password']) ? $_POST['password'] : false;
+    $nombre = isset($_POST['nombre']) ?  mysqli_real_escape_string($db, $_POST['nombre'])  : false;
+    $apellidos = isset($_POST['apellidos']) ? mysqli_real_escape_string($db, $_POST['apellidos']) : false;
+    $email = isset($_POST['email']) ? mysqli_real_escape_string($db, $_POST['email']) : false;
+    $password = isset($_POST['password']) ? mysqli_real_escape_string($db, $_POST['password']) : false;
 
 }
 
@@ -75,7 +78,8 @@ if( count($errores) == 0 ){
     $sql = "INSERT INTO usuarios  VALUES(null, '$nombre', '$apellidos', '$email', '$password_segura', CURDATE())";
     $guardar = mysqli_query( $db, $sql );
 
-   
+    /*var_dump(mysqli_error($db));
+    die();*/
 
     if($guardar){
         $_SESSION['completado'] = "El Registro se ha completado con Exito";
